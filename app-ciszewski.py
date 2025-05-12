b95433db67024457042e9262aeb4e0bc51230a1f
Ert-zal <Aarxn.ci@gmail.com>
2025-05-12 14:23:00 +0200

    modifier le message de l'application

--- app-ciszewski.py
+++ app-ciszewski.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, ALED WTF DEVOPS NANI"
+    return "Hello, ALED WTF DEVOPS NANI\n
+coucou"
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5000)
